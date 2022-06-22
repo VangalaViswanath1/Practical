4f8e075c053a0f11b4c618c3d39a5721b745f8c6
Vangala Viswanath <98110836+VangalaViswanath1@users.noreply.github.com>
2022-06-22 13:44:09 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,8 @@
+
+class First{
+        public static void main(String[] args){
+	System.out.println("Name: Viswa");
+	System.out.println("Course: Full Stack Developer ");
+	System.out.println("Stream: BCA");
+	}
+}
