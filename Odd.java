a49a9f5dfa3c30f3ab1fa68291ec0202e1b8ba30
Vangala Viswanath <98110836+VangalaViswanath1@users.noreply.github.com>
2022-06-27 18:55:29 +0530

    Top five Odd numbers

---
+++ Odd.java
@@ -0,0 +1,10 @@
+public class Odd {
+    public static void main(String[] args){
+        int i = 1;
+        System.out.println("Top five Odd numbers:");
+        do{
+            System.out.println(i);
+            i = i + 2;
+        }while(i<10);
+    }
+}
