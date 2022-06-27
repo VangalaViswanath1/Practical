e492a544349b9ebe57f8f234c036a016b9aa2a76
Vangala Viswanath <98110836+VangalaViswanath1@users.noreply.github.com>
2022-06-27 18:43:19 +0530

    Adding first ten natural numbers.

---
+++ AddTenNumbers.java
@@ -0,0 +1,9 @@
+public class AddTenNumbers {
+    public static void main(String[] args){
+        int i = 0;
+        for(int num = 1; num<=10; num++ ){
+            i = i+num;
+        }
+        System.out.println("The sum of Top 10 number is: "+ i );
+    }
+}
